13d05152ece24000f120e63f2524651c5d9f7108
Julia <email96123@gmail.com>
2024-11-11 16:32:31 -0700

    randomize horizontal or vertical

--- classes/bomb.py
+++ classes/bomb.py
@@ -7,18 +7,19 @@ class Bomb(GameObject):
 
     def __init__(self,):
         super(Bomb, self).__init__(0, 0, f'assets/bomb.png')
-        self.dx = 0
-        self.dy = (randint(0, 200) / 100) + 1
+        self.coord_values = choice([[0, (randint(0, 200) / 100) + 1, choice(
+            LANES), -64], [(randint(0, 200) / 100) + 1, 0, -64, choice(LANES)]])
+        self.dx = self.coord_values[0]
+        self.dy = self.coord_values[1]
         self.reset()  # call reset here!
 
     def move(self):
         self.x += self.dx
         self.y += self.dy
-        # Check the y position of the bomb
-        if self.y > 500:
+        if self.y > 500 or self.x > 500:
             self.reset()
 
     # add a new method
     def reset(self):
-        self.x = choice(LANES)
-        self.y = -64
+        self.x = self.coord_values[2]
+        self.y = self.coord_values[3]
