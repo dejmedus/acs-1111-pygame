6850bfb772a519323fc61e34fd1b23ab8e43f475
Julia <email96123@gmail.com>
2024-11-11 17:11:46 -0700

    update object classes for readability

--- classes/bomb.py
+++ classes/bomb.py
@@ -4,14 +4,21 @@ from classes.game_object import GameObject
 
 
 class Bomb(GameObject):
-
     def __init__(self,):
         super(Bomb, self).__init__(0, 0, f'assets/bomb.png')
-        self.coord_values = choice([[0, (randint(0, 200) / 100) + 1, choice(
-            LANES), -64], [(randint(0, 200) / 100) + 1, 0, -64, choice(LANES)]])
-        self.dx = self.coord_values[0]
-        self.dy = self.coord_values[1]
+        self.coord_values = self.generate_coords()
         self.reset()  # call reset here!
+
+    def generate_coords(self):
+        def get_random():
+            return (randint(0, 200) / 100) + 1
+
+        coord_options = {
+            "horizontal": [get_random(), 0, -64, choice(LANES)],
+            "vertical": [0, get_random(), choice(LANES), -64]
+        }
+
+        return coord_options[choice(['vertical', 'horizontal'])]
 
     def move(self):
         self.x += self.dx
@@ -21,5 +28,8 @@ class Bomb(GameObject):
 
     # add a new method
     def reset(self):
+        self.coord_values = self.generate_coords()
+        self.dx = self.coord_values[0]
+        self.dy = self.coord_values[1]
         self.x = self.coord_values[2]
         self.y = self.coord_values[3]
--- classes/fruit.py
+++ classes/fruit.py
@@ -11,11 +11,19 @@ class FruitType:
 class Fruit(GameObject):
     def __init__(self, fruit: FruitType):
         super(Fruit, self).__init__(0, 0, f'assets/{fruit}.png')
-        self.coord_values = choice([[0, (randint(0, 200) / 100) + 1, choice(
-            LANES), -64], [(randint(0, 200) / 100) + 1, 0, -64, choice(LANES)]])
-        self.dx = self.coord_values[0]
-        self.dy = self.coord_values[1]
+        self.coord_values = self.generate_coords()
         self.reset()  # call reset here!
+
+    def generate_coords(self):
+        def get_random():
+            return (randint(0, 200) / 100) + 1
+
+        coord_options = {
+            "horizontal": [get_random(), 0, -64, choice(LANES)],
+            "vertical": [0, get_random(), choice(LANES), -64]
+        }
+
+        return coord_options[choice(['vertical', 'horizontal'])]
 
     def move(self):
         self.x += self.dx
@@ -25,5 +33,8 @@ class Fruit(GameObject):
 
     # add a new method
     def reset(self):
+        self.coord_values = self.generate_coords()
+        self.dx = self.coord_values[0]
+        self.dy = self.coord_values[1]
         self.x = self.coord_values[2]
         self.y = self.coord_values[3]
